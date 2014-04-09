c26c6626b21af647d0c00e91774c8bcaead70c03
John Mellows <jmellowsjnr@gmail.com>
2014-04-09 15:37:33 +1200

    time code for video

--- client/js/script.js
+++ client/js/script.js
@@ -1,28 +1,30 @@
 Template.video.rendered = function(){
 
-var iframe = $('#player1')[0];
-var player = $f(iframe);
+	var iframe = $('#player1')[0];
+	var player = $f(iframe);
+	//get time
+	var time = function(){
+		player.addEvent('playProgress', function (data) {
+			return data++;
+		});
+	};
+	
+	player.addEvent("ready", function () {
+	player.addEvent("play", function () {
+			
+		var totaltime = time();
+		if (totaltime !== 0) {
+			console.log(totaltime);
+		}
+		else{
+			console.log('Nothing');
+		}
+		
+      });
 
-    player.addEvent("ready", function () {
-        
-        player.addEvent("play", function () {
-            mixpanel.track("Video started");
-        });
-
-        player.addEvent("pause", function () {
-            mixpanel.track("Video paused");
-        });
-
-        /*player.addEvent("playProgress", function (data) {
-            var time = data;
-            console.log(time);
-        });*/
-
-        player.addEvent("finish", function () {
-		mixpanel.track("Video finished");
-        });
-    });
+	});
 };
+
 //backup
 /*var iframe = $('#player1')[0];
 var player = $f(iframe);
